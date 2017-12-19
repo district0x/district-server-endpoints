402bac70108c6df42fe1637d3eb840e871f43d2e
madvas <matus.lestan@gmail.com>
2017-12-19 09:05:44 +0100

    update config dep

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.cognitect/transit-cljs "0.8.243"]
                  [com.taoensso/timbre "4.10.0"]
-                 [district0x/district-server-config "1.0.0"]
+                 [district0x/district-server-config "1.0.1"]
                  [mount "0.1.11"]
                  [org.clojure/clojurescript "1.9.946"]]
 
